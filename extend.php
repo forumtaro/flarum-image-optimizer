54377a9ca185173c609d5932e69d0d90eea66387
forumtaro <fej.muz@gmail.com>
2026-05-04 11:39:10 +0300

    Update extend.php

--- extend.php
+++ extend.php
@@ -1,15 +1,12 @@
 <?php
 
-namespace Custom\ImageOptimizer;
+namespace Foumtaro\ImageOptimizer;
 
 use Flarum\Extend;
 
 return [
     (new Extend\Frontend('forum'))
         ->js(__DIR__.'/js/dist/extension.js'),
-    
-    (new Extend\Frontend('admin'))
-        ->js(__DIR__.'/js/dist/admin.js'),
     
     new Extend\Locales(__DIR__.'/resources/locale'),
 ];
